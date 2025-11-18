4fd6921c173200f5a3a6d8cafd1f688f7fc9e0ba
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-11-18 15:33:39 +0200

    Basic map rendering

--- viewer/main.go
+++ viewer/main.go
@@ -5,21 +5,49 @@ import (
 	"flag"
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"io"
 	"net/http"
+	"slices"
 )
 
 import . "hive-arena/common"
 
+const Dx = 32
+const Dy = 16
+
+var Tile1 *ebiten.Image
+
 type Viewer struct {
+	Game *PersistedGame
 }
 
 func (viewer *Viewer) Update() error {
 	return nil
 }
 
-func (viewer *Viewer) Draw(screen *ebiten.Image) {
+type CoordHex struct {
+	Coords Coords
+	Hex    *Hex
+}
 
+func (viewer *Viewer) Draw(screen *ebiten.Image) {
+	state := viewer.Game.History[0].State
+
+	hexes := []CoordHex{}
+	for coords, hex := range state.Hexes {
+		hexes = append(hexes, CoordHex{coords, hex})
+	}
+	slices.SortFunc(hexes, func(a, b CoordHex) int {
+		return a.Coords.Row - b.Coords.Row
+	})
+
+	for _, h := range hexes {
+
+		opt := ebiten.DrawImageOptions{}
+		opt.GeoM.Translate(float64(Dx*h.Coords.Col/2), float64(Dy*h.Coords.Row))
+		screen.DrawImage(Tile1, &opt)
+	}
 }
 
 func (viewer *Viewer) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -57,6 +85,10 @@ func GetURL(url string) *PersistedGame {
 	return &game
 }
 
+func LoadResources() {
+	Tile1, _, _ = ebitenutil.NewImageFromFile("tile.png")
+}
+
 func main() {
 	url := flag.String("url", "", "URL of the history file to view")
 	flag.Parse()
@@ -73,11 +105,15 @@ func main() {
 
 	fmt.Println(game)
 
+	LoadResources()
+
 	ebiten.SetWindowSize(1024, 768)
 	ebiten.SetWindowTitle("Hive Arena Viewer")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	viewer := &Viewer{}
+	viewer := &Viewer{
+		Game: game,
+	}
 	err := ebiten.RunGame(viewer)
 
 	if err != nil {
